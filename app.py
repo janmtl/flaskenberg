853e45adbc726634a39f439e1cb2c1d71672521d
Jan Florjanczyk <jan.florjanczyk@gmail.com>
2014-04-06 12:59:45 -0700

    Got sqlite3 working. Got bower set up.
    Project is going to use Angular-Bootstrap for front-end.

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+import flask
+import flask.ext.sqlalchemy
+import flask.ext.restless
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = flask.Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db/flaskenberg.db')
+db = flask.ext.sqlalchemy.SQLAlchemy(app)
 
-if __name__ == "__main__":
-    app.run()
+users_tasks = db.Table('users_tasks',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('task_id', db.Integer, db.ForeignKey('task.id'))
+)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hash_id = db.Column(db.Unicode, unique=True)
+    gender = db.Column(db.Unicode)
+    age = db.Column(db.Integer)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hash_id = db.Column(db.Unicode, unique=True)
+    users = db.relationship('User', secondary=users_tasks , backref=db.backref('tasks', lazy='dynamic'))
+
+# Create the database tables.
+db.create_all()
+
+# Create the Flask-Restless API manager.
+manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
+
+# Create API endpoints, which will be available at /api/<tablename>
+manager.create_api(User, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Task, methods=['GET'])
+
+# start the flask loop
+app.run()
