20753093abd63e499cc39347b7b5b1e3e5434395
Jan Florjanczyk <jan.florjanczyk@gmail.com>
2014-04-29 20:10:12 -0700

    Clean-up

--- config.py
+++
@@ -1,3 +0,0 @@
-app.config['SECRET_KEY'] = 'temporary_secret_key' # make sure to change this
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db/flaskenberg.db')
