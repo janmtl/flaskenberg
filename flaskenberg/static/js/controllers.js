80cbc6b60a05baf5574943d41c3720d6d62708e4
Jan Florjanczyk <jan.florjanczyk@gmail.com>
2014-07-01 18:22:43 -0700

    Moves onto the next question! but now throws at the end of the tasks assigned to the user

--- flaskenberg/static/js/controllers.js
+++ flaskenberg/static/js/controllers.js
@@ -5,16 +5,21 @@ var flaskenbergControllers = angular.module('flaskenbergControllers', []);
 
 flaskenbergControllers.controller('SurveyCtrl', ['$scope', 'User', 'Task', 'Question', 'Answer',
   function($scope, User, Task, Question, Answer) {
-
-    //Get the next question
-    User.next({userId: 1}, function(res){
-      $scope.answer   = Answer.get({answerId: res.answer_id});
-      $scope.task     = Task.get({taskId: res.task_id});
-      $scope.question = Question.get({questionId: res.question_id});
-    });
+    
+    var init = function(){
+      //Get the next question
+      User.next({userId: 1}, function(res){
+        $scope.answer   = Answer.get({answerId: res.answer_id});
+        $scope.task     = Task.get({taskId: res.task_id});
+        $scope.question = Question.get({questionId: res.question_id});
+      });
+    };
     
     //Submit
     $scope.submit = function(){
       Answer.save($scope.answer);
+      init();
     };
+
+    init();
   }]);
